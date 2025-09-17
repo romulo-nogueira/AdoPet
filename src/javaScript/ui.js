d4969f2f5ddc547d0608833d6741376afe00651b
Romulo Nogueira <romulonogueira1607@gmail.com>
2025-09-16 21:19:49 -0300

    criado modal para cadastro de caes

--- src/javaScript/ui.js
+++ src/javaScript/ui.js
@@ -6,7 +6,7 @@ const ui = {
 
         try {
             const pets = await api.obterPets();
-            pets.forEach(item => {
+             pets.forEach(ui.adicionarElements) /*=> {
                 listPets.innerHTML += ` 
                  <li data-id ="${item.id}" >
                   <img src="${item.imagem}" alt="${item.imagem}"/>
@@ -17,11 +17,35 @@ const ui = {
                 </li>
                 `
                
-            })
+            }) */
         } catch (error) {
             alert("Falha ao renderizar a api")
             throw error
         }
+    },
+    adicionarElements(pets){
+        const listPets = document.getElementById('pets-list');
+        const li = document.createElement('li');
+        li.setAttribute('data-id', pets.id);
+
+        const img =document.createElement('img');
+        img.src = pets.imagem;
+        img.alt = ` imagem de animal: ${pets.nome}`;
+        /* li.appendChild(img); */
+
+        const h2 = document.createElement('h2');
+        h2.innerText = pets.nome;
+
+        const p = document.createElement('p');
+        p.innerText = pets.raca;
+
+        const span = document.createElement('span');
+        span.innerText = pets.descricao;
+
+        const button = document.createElement('button');
+        button.innerText = "Quero Adotar";
+        li.append(img, h2, p, span, button)
+        listPets.appendChild(li)
     }
 }
 
